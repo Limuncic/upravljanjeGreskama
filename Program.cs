ec4aac2cfbb1f7f3507017bc13aa01b4dcc03c99
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-10-18 08:25:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljanjeGreskama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int godR;
+            bool kontrola = false;
+
+            
+            do {
+                Console.Write("Upisi svoju godinu rođenja: ");
+                try
+                {
+      
+                    godR = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("Roden si: " + godR + " god.");
+                    kontrola = true;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }while(kontrola==false);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
